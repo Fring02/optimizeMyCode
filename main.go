ec84431e1869a3e49b76d2f01fc144df3c0e0e05
Sultan <hasenovsultanbek@gmail.com>
2021-06-02 21:55:22 +0600

    Final optimization and refactoring

--- main.go
+++ main.go
@@ -14,170 +14,140 @@ import (
 
 func main() {
 	start := time.Now()
-
 	WordCounter(ioutil.Discard)
-
 	elapsed := time.Since(start)
 	log.Printf("WordCounter took %s", elapsed)
 
 }
 
-type matcher struct {
-	byteArray []byte
-	occurence uint
+type Matcher struct {
+	Word []byte
+	Occurrence uint
 }
-
-//function for searching the slice of bytes in the slice of slice of bytes
-func isUsed(arr *[]matcher, word *[]byte) int {
+func sort(words *[]Matcher, first int, last int) {
+	left, right := first, last
+	pivot := (*words)[(left+right)/2].Occurrence
+	for left <= right {
+		for (*words)[left].Occurrence < pivot {
+			left++
+		}
+		for (*words)[right].Occurrence > pivot {
+			right--
+		}
+		if left <= right {
+			(*words)[left], (*words)[right] = (*words)[right], (*words)[left]
+			left++
+			right--
+		}
+	}
+	if first < right {
+		sort(words, first, right)
+	}
+	if left < last {
+		sort(words, left, last)
+	}
+}
+func indexOfWord(arr *[]Matcher, word *[]byte) int {
 	for i := 0; i < len(*arr); i++ {
-		if bytes.Compare((*arr)[i].byteArray, *word) == 0 {
+		if bytes.Equal((*arr)[i].Word, *word) {
 			return i
 		}
 	}
 	return -1
 }
-
+func indexOfMatcher(arr *[]Matcher, word *Matcher) int {
+	for i := 0; i < len(*arr); i++ {
+		if bytes.Equal((*arr)[i].Word, (*word).Word) {
+			return i
+		}
+	}
+	return -1
+}
 func WordCounter(out io.Writer) {
-
-	data, err := ioutil.ReadFile(".\\mobydick.txt")
-
+	chars, err := ioutil.ReadFile(".\\mobydick.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	//one dimensional array for storing a single word
-	var oned []byte
-	//kind of sorted slice. In few words, it stores words
-	var sortedSlice [][]byte
-
-	size := len(data)
-
-	for i := 0; i < size-1; i++ {
-		//checking here whether a byte is a letter or a symbol
-		if data[i] >= 97 && data[i] <= 122 || data[i] >= 65 && data[i] <= 90 {
-			//and appending only symbols
-			oned = append(oned, data[i])
-			//if array does not find any letters it means that new word started
-			continue
-		}
-		if len(oned) > 0 {
-			//empty array check
-			sortedSlice = append(sortedSlice, oned)
-		}
-		oned = []byte{}
-	}
-
-	size = len(sortedSlice)
-
-	numOfCPUs := runtime.NumCPU()
-	//Slice for checked words, reading and counting already checked words cause huge overhead
-
-	var wg sync.WaitGroup
-	var mutex sync.Mutex
-
-	var matcherChannel = make(chan []matcher)
-
-	//var wordChannel = make(chan []byte, size)
-	//var occurrenceChannel = make(chan uint, size)
-
-	if numOfCPUs > 1 {
-
-		avgNumberOfLines := int(math.Round(float64(size / numOfCPUs)))
-
-		for i := 0; i < numOfCPUs; i++ {
-
-			wg.Add(1)
-
-			if diff := size - avgNumberOfLines; diff < 0 {
-				avgNumberOfLines -= diff
+	var word []byte
+	var words [][]byte
+	size := len(chars)
+	//get all words from chars and store in words array
+	for i := 0; i < size - 1; i++ {
+		//if char is a letter
+		if chars[i] >= 97 && chars[i] <= 122 || chars[i] >= 65 && chars[i] <= 90 {
+			word = append(word, chars[i])
+		} else {
+			//if char is not a letter -> word ends
+			if len(word) > 0 {
+				words = append(words, word)
+				word = []byte{}
 			}
-
-			size -= avgNumberOfLines
-
-			go func(matchChannel chan []matcher,
-				wg *sync.WaitGroup, mutex *sync.Mutex,
-				text [][]byte, numberOfLines int, firstIndex int, lastIndex int) {
-
+		}
+	}
+	numOfCPUs := runtime.NumCPU()
+	var wg sync.WaitGroup
+	var matcherChannel = make(chan []Matcher)
+	size = len(words)
+	avgNumberOfLines := int(math.Floor(float64(size)/float64(numOfCPUs)))
+	wg.Add(numOfCPUs)
+		for i := 0; i < numOfCPUs; i++ {
+			if size - avgNumberOfLines > 0 {
+				size -= avgNumberOfLines
+			}
+			firstIndex := i * avgNumberOfLines
+			secondIndex := (i + 1) * avgNumberOfLines - 1
+			if i + 1 == numOfCPUs {
+				secondIndex += size
+			}
+			go func(matchChannel chan []Matcher, wg *sync.WaitGroup,
+				text [][]byte, firstIndex int, lastIndex int) {
 				defer wg.Done()
-
-				var matchArray []matcher
-
-				matchArray = append(matchArray, matcher{
-					byteArray: text[firstIndex],
-					occurence: 1,
+				var matchArray []Matcher
+				matchArray = append(matchArray, Matcher{
+					Word:       text[firstIndex],
+					Occurrence: 1,
 				})
-
 				for i := firstIndex + 1; i <= lastIndex; i++ {
-					index := isUsed(&matchArray, &text[i])
+					index := indexOfWord(&matchArray, &text[i])
 					if index == -1 {
-						matchArray = append(matchArray, matcher{
-							byteArray: text[i],
-							occurence: 1,
+						matchArray = append(matchArray, Matcher{
+							Word:       text[i],
+							Occurrence: 1,
 						})
 					} else {
-						matchArray[index].occurence += 1
+						matchArray[index].Occurrence++
 					}
 				}
-
 				matcherChannel <- matchArray
-
-			}(matcherChannel, &wg, &mutex,
-				sortedSlice, avgNumberOfLines,
-				i*avgNumberOfLines, (i+1)*avgNumberOfLines-1)
-
+			}(matcherChannel, &wg, words, firstIndex, secondIndex)
 		}
-
-	}
-
-	var matchedArray [][]matcher
-
 	go func() {
 		wg.Wait()
 		close(matcherChannel)
 	}()
-
+	var finalWords []Matcher
 	for request := range matcherChannel {
-		matchedArray = append(matchedArray, request)
+		finalWords = append(finalWords, request...)
 	}
-
-	fmt.Println(len(matchedArray))
-
-	//usedWords = append(usedWords, sortedSlice[0])
-	//occurrenceSlice = append(occurrenceSlice, 1)
-	//
-	//for i := 1; i< size; i++{
-	//	index = isUsed(&usedWords, &sortedSlice[i])
-	//	if index == -1{
-	//		usedWords = append(usedWords, sortedSlice[i])
-	//		occurrenceSlice = append(occurrenceSlice, 1)
-	//	}else {
-	//		occurrenceSlice[index] += 1
-	//	}
-	//}
-	//
-	//size = len(occurrenceSlice)
-	//
-	////bubble sort for sorting arrays by occurrence
-	//for i := 0; i < size-1; i++ {
-	//	for j := i+1; j < size-i-1; j++ {
-	//		if occurrenceSlice[j] > occurrenceSlice[j+1] {
-	//
-	//			temp := occurrenceSlice[j]
-	//			occurrenceSlice[j] = occurrenceSlice[j+1]
-	//			occurrenceSlice[j+1] = temp
-	//
-	//			byteSlice := usedWords[j]
-	//			usedWords[j] = usedWords[j+1]
-	//			usedWords[j+1] = byteSlice
-	//
-	//		}
-	//	}
-	//}
-	//
-	////printing used words
-	//for i := 0; i < 25; i++ {
-	//	print(string(usedWords[size-i-1]) + " ")
-	//	println(occurrenceSlice[size-i-1])
-	//}
-	//fmt.Fprintln(out, "fuck")
+	var cache []Matcher
+	var index int
+	cache = append(cache, Matcher{
+		Word:       finalWords[0].Word,
+		Occurrence: finalWords[0].Occurrence,
+	})
+	size = len(finalWords)
+	for i := 1; i < size; i++{
+			index = indexOfMatcher(&cache, &finalWords[i])
+			if index == -1 {
+				cache = append(cache, finalWords[i])
+			} else {
+				(cache)[index].Occurrence += finalWords[i].Occurrence
+			}
+	}
+	size = len(cache)
+	sort(&cache, 0, len(cache) - 1)
+	for i := 0; i < 25; i++ {
+		print(string(cache[size-i-1].Word) + " ")
+		println(cache[size-i-1].Occurrence)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func BenchmarkWordCounter(b *testing.B) {
+	for i := 0; i < b.N; i++{
+		WordCounter(ioutil.Discard)
+	}
+}
